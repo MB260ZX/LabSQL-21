32244ae0e1980f0ea5c81b8e65040e7dc901cd99
MB260ZX <masterthaworawat@gmail.com>
2025-08-21 09:42:57 +0700

    Add files via upload

--- LabSQL 21 สิงหาคม 2568.sql
+++ LabSQL 21 สิงหาคม 2568.sql
@@ -1,1 +1,15 @@
-SELECT *FROM Products
+select CategoryName,ProductName,UnitPrice from Products,Categories  where Products.CategoryID = Categories.CategoryID order by CategoryName
+
+select CategoryName,ProductName,UnitPrice from Products join Categories on Products.CategoryID = Categories.CategoryID order by CategoryName
+
+select c.CategoryName,p.ProductName,UnitPrice from Products as p ,Categories as c where p.CategoryID = c.CategoryID order by CategoryName
+
+select CompanyName,OrderID from Orders as o ,Shippers as s  where s.ShipperID=o.ShipVia 
+
+select CompanyName,OrderID from Orders as o  join Shippers as s  on s.ShipperID=o.ShipVia 
+
+select p.ProductID, p.ProductName ,s.CompanyName,s.Country from Products as p join Suppliers as s  on p.SupplierID = s.SupplierID 
+
+select s.CompanyName,o.OrderID from Orders as o ,Shippers as s where s.ShipperID=o.ShipVia and OrderID = 10275
+
+select s.CompanyName,o.OrderID from Orders as o join Shippers as s  on s.ShipperID=o.ShipVia where OrderID = 10275
